e77353ca860d6c0d6cdb86f73fc3429562e37595
Micah Folsom <micahfolsom@gmail.com>
2021-10-12 18:43:15 -0700

    Add lecture15 code

---
+++ lecture15/B1/src/B1EventAction.cc
@@ -0,0 +1,88 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1EventAction.cc
+/// \brief Implementation of the B1EventAction class
+
+#include "B1EventAction.hh"
+#include "B1RunAction.hh"
+#include "G4Run.hh"
+
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1EventAction::B1EventAction(B1RunAction* runAction)
+: G4UserEventAction(),
+  fRunAction(runAction),
+  fEdep(0.)
+{} 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1EventAction::~B1EventAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::BeginOfEventAction(const G4Event*)
+{    
+  fEdep = 0.;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1EventAction::EndOfEventAction(const G4Event* event)
+{   
+  // accumulate statistics in run action
+  fRunAction->AddEdep(fEdep);
+
+
+  /*--!! Start of NE697 code !!--*/
+  // Just print every 10000
+  /*auto ievent = event->GetEventID();
+  if (ievent % 10000 == 0) {
+    G4cout << "Processing event " << ievent << G4endl;
+  }*/
+
+  // Print every 5%
+  auto run_man = G4RunManager::GetRunManager();
+  auto run = dynamic_cast<G4Run*>(run_man->GetNonConstCurrentRun());
+  auto ntotal = run->GetNumberOfEventToBeProcessed();
+  auto ievent = event->GetEventID();
+  // Report status every 5% or so
+  int rep_freq = (int)(ntotal / 20);
+  if (rep_freq == 0) {
+    rep_freq = 1;
+  }
+  if (ievent % rep_freq == 0) {
+    G4cout << "Processing event " << ievent << " ("
+      << 100.0 * (double)ievent / (double)ntotal << " %)" << G4endl;
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
